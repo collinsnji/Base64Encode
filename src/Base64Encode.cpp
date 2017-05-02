79fc6bcb6865b5caa3f46b1cb4c588523a68b3c2
collinsnji <collins.geek@gmail.com>
2017-05-02 22:43:50 +0100

    Test changes

--- src/Base64Encode.cpp
+++ src/Base64Encode.cpp
@@ -1,19 +1,11 @@
-#include "../include/Base64Encode.h"
-#include "../include/BaseEncode.h"
+// #include "../include/Base64Encode.h"
+#include "../include/BaseConverter.h"
 #include <iostream>
 
 using namespace std;
 
-int main(){	
-	Base64Encode Q;
-	Encode S;
-	Encode R;
-
-	int N = Q.Convert_Ascii('A');
-	string T = Q.Convert_Bin(N);
-	string V = S.BYTE_Split(T);
-	cout << R.Binary_convert(V) << endl;
-
-	// cout << "Initiating splinter sequence..." << endl;
+int main(){
+	BaseConvert Q;
+	cout << Q.to_binary("This is a simple test") << endl;
 	return 0;
 }
